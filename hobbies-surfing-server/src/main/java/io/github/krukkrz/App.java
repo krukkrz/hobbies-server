3118e0d25f96c519927e55ed90022ba76267e139
Krzysztof Kruk <krzysztof.kruk@loomispay.com>
2022-05-12 19:51:28 +0200

    git ignore

--- hobbies-surfing-server/src/main/java/io/github/krukkrz/App.java
+++ hobbies-surfing-server/src/main/java/io/github/krukkrz/App.java
@@ -6,5 +6,7 @@ public class App {
     public static void main(String[] args) {
         var app = Javalin.create().start(8081);
         app.get("/hello", ctx -> ctx.result("Hello world!"));
+
+        //handler - first should call keycloak authorization endpoint - based on response it should continue or return 401
     }
 }
